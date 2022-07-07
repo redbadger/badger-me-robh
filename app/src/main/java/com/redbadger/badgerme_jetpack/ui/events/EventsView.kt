784a52787a8b4c2ba5f3fc344d612b3fb68da6ea
Rob Hannan <rob.hannan@red-badger.com>
2022-07-07 15:44:41 +0100

    Add bottom sheet to events view

--- app/src/main/java/com/redbadger/badgerme_jetpack/ui/events/EventsView.kt
+++ app/src/main/java/com/redbadger/badgerme_jetpack/ui/events/EventsView.kt
@@ -4,15 +4,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,9 +23,11 @@ import com.redbadger.badgerme_jetpack.ui.theme.BadgerMe_JetpackTheme
 import com.redbadger.badgerme_jetpack.util.BadgerEvent
 import com.redbadger.badgerme_jetpack.util.BadgerInterest
 import com.redbadger.badgerme_jetpack.util.BadgerUser
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.LocalDateTime
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun BadgerEventsView(
     navHostController: NavHostController?,
@@ -35,110 +37,159 @@ fun BadgerEventsView(
 ) {
     val tabs = listOf("Today", "Upcoming")
     val currentTab = remember { mutableStateOf(0) }
-    Box {
-        Column () {
-            Row(Modifier.fillMaxWidth()) {
-                Column(
-                    Modifier
-                        .background(color = Color.White)
-                        .padding(start = 16.dp, top = 7.5.dp, end = 16.dp)) {
-                    Row(Modifier.fillMaxWidth()) {
-                        Text(
-                            text = "Badger events",
-                            style = MaterialTheme.typography.h3
-                        )
-                    }
-                    Row(
-                        modifier = Modifier
-                            .height(48.dp)
-                            .background(color = Color.White)
-                            .fillMaxWidth()
-                    ) {
-                        TabRow(
-                            selectedTabIndex = currentTab.value,
-                            backgroundColor = Color.White,
-                            contentColor = MaterialTheme.colors.primary,
-                            modifier = Modifier.width(210.dp)
-                        ) {
-                            tabs.forEachIndexed { index, it ->
-                                Tab(
-                                    selected = true,
-                                    onClick = {
-                                        viewModel.timeFilter.value = it
-                                        currentTab.value = index
-                                    },
-                                    text = {
-                                        Text(
-                                            text = it,
-                                            style = MaterialTheme.typography.button,
-                                            color = Color.Black
-                                        )
+    val interests = listOf<BadgerInterest>()
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
+    )
+    val coroutineScope = rememberCoroutineScope()
+    BottomSheetScaffold(
+        scaffoldState = bottomSheetScaffoldState,
+        sheetShape = RoundedCornerShape(8.dp),
+        sheetContent = {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            ) {
+                Column {
+                    Box {
+                        Row(Modifier.fillMaxWidth().padding(16.dp), horizontalArrangement = Arrangement.Start) {
+                            Image(
+                                painter = painterResource(
+                                    id = R.drawable.controls_close
+                                ),
+                                contentDescription = "settings",
+                                modifier = Modifier
+                                    .size(24.dp)
+                                    .clickable {
+                                        coroutineScope.launch {
+                                            bottomSheetScaffoldState.bottomSheetState.collapse()
+                                        }
                                     }
-                                )
-                            }
+                            )
                         }
-                        Column(
-                            modifier = Modifier.fillMaxHeight(),
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.End
-                            ) {
-                                Image(
-                                    painter = painterResource(
-                                        id = R.drawable.preferences__state_enabled
-                                    ),
-                                    contentDescription = "settings",
-                                    modifier = Modifier
-                                        .size(24.dp)
-                                        .clickable { /*TODO*/ }
-                                )
-                            }
+                        Row(Modifier.fillMaxWidth().padding(vertical = 16.dp), horizontalArrangement = Arrangement.Center) {
+                            Text(
+                                text = "Filter by interests (${interests.size})",
+                                style = MaterialTheme.typography.h5,
+                            )
                         }
                     }
                 }
             }
-            Row {
-                Column(modifier = Modifier
-                    .fillMaxHeight()
-                    .padding(horizontal = 16.dp),
-                    verticalArrangement = Arrangement.Top
-                ) {
-                    EventsList(
-                        getEvents().filter {
-                            when (viewModel.timeFilter.value) {
-                                "Today" -> {
-                                    viewModel.tomorrow.value = -1
-                                    viewModel.thisWeek.value = -1
-                                    viewModel.nextWeek.value = -1
-                                    viewModel.later.value = -1
-
-                                    LocalDateTime
-                                        .parse(it.startTime).toLocalDate()
-                                        .isEqual(LocalDate.now())
+        }, sheetPeekHeight = 0.dp
+    ) {
+        Box {
+            Column() {
+                Row(Modifier.fillMaxWidth()) {
+                    Column(
+                        Modifier
+                            .background(color = Color.White)
+                            .padding(start = 16.dp, top = 7.5.dp, end = 16.dp)
+                    ) {
+                        Row(Modifier.fillMaxWidth()) {
+                            Text(
+                                text = "Badger events",
+                                style = MaterialTheme.typography.h3
+                            )
+                        }
+                        Row(
+                            modifier = Modifier
+                                .height(48.dp)
+                                .background(color = Color.White)
+                                .fillMaxWidth()
+                        ) {
+                            TabRow(
+                                selectedTabIndex = currentTab.value,
+                                backgroundColor = Color.White,
+                                contentColor = MaterialTheme.colors.primary,
+                                modifier = Modifier.width(210.dp)
+                            ) {
+                                tabs.forEachIndexed { index, it ->
+                                    Tab(
+                                        selected = true,
+                                        onClick = {
+                                            viewModel.timeFilter.value = it
+                                            currentTab.value = index
+                                        },
+                                        text = {
+                                            Text(
+                                                text = it,
+                                                style = MaterialTheme.typography.button,
+                                                color = Color.Black
+                                            )
+                                        }
+                                    )
                                 }
-                                "Upcoming" -> {
-                                    viewModel.tomorrow.value = -1
-                                    viewModel.thisWeek.value = -1
-                                    viewModel.nextWeek.value = -1
-                                    viewModel.later.value = -1
-
-                                    LocalDateTime
-                                        .parse(it.startTime).toLocalDate()
-                                        .isAfter(LocalDate.now())
-                                }
-                                else -> false
                             }
-                        }.sortedBy { it.startTime },
-                        BadgerUser(
-                        "1",
-                        "Hugh",
-                        "Mann",
-                        "hugh.mann@red-badger.com"
-                        ),
-                        viewModel
-                    )
+                            Column(
+                                modifier = Modifier.fillMaxHeight(),
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.End
+                                ) {
+                                    Image(
+                                        painter = painterResource(
+                                            id = R.drawable.preferences__state_enabled
+                                        ),
+                                        contentDescription = "settings",
+                                        modifier = Modifier
+                                            .size(24.dp)
+                                            .clickable {
+                                                coroutineScope.launch {
+                                                    bottomSheetScaffoldState.bottomSheetState.expand()
+                                                }
+                                            }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+                Row {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(horizontal = 16.dp),
+                        verticalArrangement = Arrangement.Top
+                    ) {
+                        EventsList(
+                            getEvents().filter {
+                                when (viewModel.timeFilter.value) {
+                                    "Today" -> {
+                                        viewModel.tomorrow.value = -1
+                                        viewModel.thisWeek.value = -1
+                                        viewModel.nextWeek.value = -1
+                                        viewModel.later.value = -1
+
+                                        LocalDateTime
+                                            .parse(it.startTime).toLocalDate()
+                                            .isEqual(LocalDate.now())
+                                    }
+                                    "Upcoming" -> {
+                                        viewModel.tomorrow.value = -1
+                                        viewModel.thisWeek.value = -1
+                                        viewModel.nextWeek.value = -1
+                                        viewModel.later.value = -1
+
+                                        LocalDateTime
+                                            .parse(it.startTime).toLocalDate()
+                                            .isAfter(LocalDate.now())
+                                    }
+                                    else -> false
+                                }
+                            }.sortedBy { it.startTime },
+                            BadgerUser(
+                                "1",
+                                "Hugh",
+                                "Mann",
+                                "hugh.mann@red-badger.com"
+                            ),
+                            viewModel
+                        )
+                    }
                 }
             }
         }
